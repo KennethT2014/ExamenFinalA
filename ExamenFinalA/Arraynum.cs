46015ebbc8873f383954ab64d29ad99885b3259d
kennethT2014 <kenneth.teller2014@gmail.com>
2022-02-24 11:48:04 -0600

    Add project files.

---
+++ ExamenFinalA/Arraynum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace ExamenFinalA
+{
+    class Arraynum
+    {
+        static void Main(string[] args)
+        {
+            ArrayList mainList = new ArrayList();
+
+
+
+            mainList.Add(5);
+            mainList.Add(4);
+            mainList.Add(0);
+            mainList.Add(4);
+            mainList.Add(5);
+
+            foreach (var num in mainList)
+            {
+                Console.WriteLine(" \n Lista de numeros: " + num);
+            }
+        }
+
+    }
+}
